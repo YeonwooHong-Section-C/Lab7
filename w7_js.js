f17d313f4e892f9461d6d7f431e01367e32a26bc
Yeonu <156844831+YeonwooHong-Section-C@users.noreply.github.com>
2024-02-24 18:26:32 -0500

    Modify: change function forEach to for loop - setAttribute to dot notation - let to const

--- w7_js.js
+++ w7_js.js
@@ -1,43 +1,35 @@
 document.getElementById('submit').addEventListener("click", (event) => {
-    let task = document.createElement('li')
+    const task = document.createElement('li');
     document.getElementById('list').appendChild(task);
-    task.setAttribute('id', "task");  
+    task.id = 'task';
 
-    let checkbox = document.createElement('input')
-    checkbox.setAttribute("type", "checkbox")
-    checkbox.setAttribute("class", "checkbox")
-    task.appendChild(checkbox) 
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
+    task.appendChild(checkbox);
 
     task.innerHTML += document.getElementById('input').value;
 
-
     const clicked_checkboxes = document.querySelectorAll('.checkbox');
 
-    clicked_checkboxes.forEach(function(checkbox) {
+    for (i = 0; i < clicked_checkboxes.length; i++){
+        const checkbox = clicked_checkboxes[i]
+
         checkbox.addEventListener('change', function() {
             if (checkbox.checked) {
                 checkbox.parentNode.style.textDecoration = 'line-through';
+            } else {
+                checkbox.parentNode.style.textDecoration = '';
             }
-      });
-    })
-
+        })
+    }
 });
 
 
 document.getElementById('remove').addEventListener("click", (event) => {
     const checkboxes = document.querySelectorAll('.checkbox:checked');
-    console.log(checkboxes)
+    
     for (i = 0; i < checkboxes.length; i++){
         document.getElementById('list').removeChild(checkboxes[i].parentNode);  
     }
 });
-
-
-
-// let myarr = [2,4,1,7];
-// myarr.push(10);
-// myarr.pop();
-// myarr.sort((a, b) => b - a); //reverse
-// for (let i = 0; i < myarr.length; i++) {
-//     console.log("the value currently is " + myarr[i] + " but the index is " + i);
-// }
